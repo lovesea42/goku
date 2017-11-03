849b888f06830467ab52044489ef97b1ad1cc7c5
夏冬_在线 <xiadong@cmos.chinamobile.com>
2017-11-03 17:38:24 +0800

    新增配置文件解析

---
+++ src/data/data.go
@@ -0,0 +1,12 @@
+package data
+
+//传输数据集合
+type data struct {
+	id string
+	ip string
+	port string
+	tp string
+	tm string		//采集时间
+	data string		//数据包
+	error string
+}
--- src/main/main.go
+++ src/main/main.go
@@ -9,10 +9,16 @@ import (
 	//"jvm"
 	//"osinfo"
 	//"file"
+	"config"
+	"fmt"
+	"encoding/json"
 )
 
 func main(){
 
+	cfg := new(config.Config)
+	cfg.InitConfig("config.ini")
+	fmt.Println(cfg.Read("default","dest"))
 	//osinfo.GetOSArch()
 	//jvm.Test()
 	//file.FileCreator()
